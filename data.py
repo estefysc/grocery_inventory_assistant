04216179bea2601b3e9d90c0a1dd9749845b2e2c
Estefania Sanchez <estefysc@gmail.com>
2024-10-11 18:38:20 -0400

    cleaned

--- data.py
+++ data.py
@@ -1,5 +1,4 @@
-# open ai api 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
-# data.py
+
 inventory = {
     'Bananas': {'quantity': 10, 'last_purchased_date': '2023-09-01', 'consumption_rate': None}
 }
